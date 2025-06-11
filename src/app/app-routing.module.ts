abd89d6422c85e67d4b2885dcc0b39c7efe72474
disendrag <disendra.g@revalsys.com>
2025-06-11 14:55:17 +0530

    Intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageModule } from './components/login-page/login-page.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: async () => (await import('./components/login-page/login-page.module')).LoginPageModule
+    // canActivate: [SiteGuardGuard]
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/constants/api-constants.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from '@angular/core';
+import { Request } from 'express';
+
+export const alertResponseTime = 10000;
+export const base64Key = 'RevalKey';
