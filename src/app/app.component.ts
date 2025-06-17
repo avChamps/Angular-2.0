c4d3b04155b036f2963ffa405e8fd89e72f31428
disendrag <disendra.g@revalsys.com>
2025-06-17 14:03:41 +0530

    Design fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,26 +25,30 @@ isDisplayHeader : boolean = true;
       .pipe(filter((event: any) => event instanceof NavigationEnd))
       .subscribe(() => {
         let currentRoute = this.activatedRoute;
-       this.isDisplayHeader = !this.router.url.includes('login') || !this.router.url.includes('/');
+  
+        // Hide header if route is '/' or includes 'login'
+        this.isDisplayHeader = !(this.router.url === '/' || this.router.url.includes('login'));
+  
         while (currentRoute.firstChild) {
           currentRoute = currentRoute.firstChild;
         }
-
+  
         const routeData = currentRoute.snapshot.data;
-
+  
         if (routeData['title']) {
-          this.titleService.setTitle(routeData['title'] + '| AV Champs');
+          this.titleService.setTitle(`${routeData['title']} | AV Champs`);
         }
-
+  
         if (routeData['description']) {
-          this.metaService.updateTag({ name: 'description', content: routeData['description' + '| AV Champs'] });
+          this.metaService.updateTag({ name: 'description', content: `${routeData['description']} | AV Champs` });
         }
-
+  
         if (routeData['keywords']) {
-          this.metaService.updateTag({ name: 'keywords', content: routeData['keywords' + '| AV Champs'] });
+          this.metaService.updateTag({ name: 'keywords', content: `${routeData['keywords']} | AV Champs` });
         }
       });
   }
+  
 
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  APIURL: "http://localhost:3000"
+  // APIURL: "http://localhost:3000"
+  APIURL : "https://nodejs-2-0-1rv9.onrender.com"
 };
