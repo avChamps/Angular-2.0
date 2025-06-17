22cf3e56486858c3da99543c877d50f5771fc58f
disendrag <disendra.g@revalsys.com>
2025-06-17 16:12:38 +0530

    home page changes

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { getProfile } from '../../constants/api-constants';
 
 @Component({
   selector: 'app-home-page',
@@ -8,8 +10,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomePageComponent {
   menuVisible = false;
+  token : any;
+  emailId : any;
+  profileData : any;
 
-  constructor(private router: Router, private actRoute : ActivatedRoute) { }
+  constructor(private router: Router, private actRoute : ActivatedRoute, private http : HttpClient,private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+      if (this.token) {
+        localStorage.setItem('jwtToken', this.token);
+      }
+    });
+
+    let emailId = localStorage.getItem('EmailId');
+    if (emailId) {
+      this.emailId = emailId;
+    }
+    this.getProfiles()
+  }
 
   toggleMenu(event: Event) {
     event.stopPropagation();
@@ -33,5 +53,22 @@ export class HomePageComponent {
 onNaviagate(router : any) {
   this.router.navigate([router])
 }
+
+getProfiles() {
+  this.http.get(getProfile, {
+    params: {
+      emailId: this.emailId || '',
+      JwtToken: this.token || ''
+    }
+  }).subscribe({
+    next: (response: any) => {
+      this.profileData = response?.profile;
+    },
+    error: (error) => {
+      console.error('Error fetching profile:', error);
+    }
+  });
+}
+
 
 }
